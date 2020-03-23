dfd2fe4d67db03030ac087df6a2e3da1c869c7f7
pacozaa <pacosarin@gmail.com>
2020-03-23 13:23:24 +0700

    make step-one

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:urbandic/DefoList.dart';
-import 'package:urbandic/WordDefinition.dart';
-import 'package:urbandic/fetchUrban.dart';
-import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -48,12 +44,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String searchTerm = '';
-
-  
-
-  // TextEditingController _controller;
 
   @override
   void initState() {
@@ -62,67 +52,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      // backgroundColor: Colors.grey,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(20.0),
-              width: 500.0,
-              child: TextField(
-                // controller: _controller,
-                onChanged: (text) {
-                  setState(() {
-                    searchTerm = text;
-                  });
-                },
-              ),
+            Container(              
             ),
             Container(
               color: Colors.green,
-              child: FutureBuilder<List<WordDefinition>>(
-                future: fetchUrban(http.Client(), searchTerm),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) print(snapshot.error);
-
-                  return snapshot.hasData
-                      ? DefoList(words: snapshot.data)
-                      : Center(child: CircularProgressIndicator());
-                },
-              ),
-              padding: EdgeInsets.all(20.0),
-              margin: EdgeInsets.all(20.0),
-              width: 500.0,
-              height: 500.0,
             )
           ],
         ),
